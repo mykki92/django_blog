7fecc87d4eacfb002ff804d262772737a97800ff
mykki92 <mike2108@hotmail.co.uk>
2023-09-10 09:42:05 +0000

    Debug false

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mykki92-djangoblog-9tbda6vp9wa.ws-eu104.gitpod.io', 'i-think-therefore-i-django', 'localhost']
 
